c5b8dc7ad827cf00d842f04d715114417c227231
jib <jib@yunk.ai>
2019-06-14 16:44:10 +0800

    票务系统微服务项目创建

---
+++ jim-cloud-ticket-client/src/main/java/com/jim/cloud/JimCloudTicketClientApplication.java
@@ -0,0 +1,13 @@
+package com.jim.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JimCloudTicketClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JimCloudTicketClientApplication.class, args);
+    }
+
+}
