0991e93c7d31a7885849854a2d024963cff3924f
jib <jib@yunk.ai>
2019-06-14 16:40:57 +0800

    用户微服务项目创建

---
+++ jim-cloud-user-client/src/main/java/com/jim/cloud/JimCloudUserClientApplication.java
@@ -0,0 +1,13 @@
+package com.jim.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JimCloudUserClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JimCloudUserClientApplication.class, args);
+    }
+
+}
