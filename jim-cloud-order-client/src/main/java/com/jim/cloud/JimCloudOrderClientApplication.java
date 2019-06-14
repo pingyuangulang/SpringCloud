d3a3e537b9b297891b2801c608ed4c77ebc19663
jib <jib@yunk.ai>
2019-06-14 16:45:45 +0800

    订单系统微服务项目创建

---
+++ jim-cloud-order-client/src/main/java/com/jim/cloud/JimCloudOrderClientApplication.java
@@ -0,0 +1,13 @@
+package com.jim.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JimCloudOrderClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JimCloudOrderClientApplication.class, args);
+    }
+
+}
